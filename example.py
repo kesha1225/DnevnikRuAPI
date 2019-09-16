23bef50a31f80f803388fc08b1885e2f2cc50caa
kesha1225 <48860626+kesha1225@users.noreply.github.com>
2019-09-16 22:01:28 +0300

    fix...

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
-from pydnevnikruapi import DiaryAPI
+from pydnevnikruapi import dnevnik
 from datetime import datetime
 
 login = "login"
 password = "password"
 
-dn = DiaryAPI(login, password)
+dn = dnevnik.DiaryAPI(login, password)
 
 print(dn.get_my_school_homework(1000002283077, datetime(2019, 9, 5), datetime(2019, 9, 15)))
 
