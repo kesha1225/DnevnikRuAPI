c7998db64bb80dd2cc307dd6c72cb004a0919d46
kesha1225 <samedov03@mail.ru>
2022-03-14 13:33:10 +0300

    constants added

---
+++ pydnevnikruapi/constants.py
@@ -0,0 +1,7 @@
+BASE_URL = "https://api.dnevnik.ru/v2/"
+LOGIN_URL = "https://login.dnevnik.ru/login/"
+RETURN_URL = (
+    "https://login.dnevnik.ru/oauth2?response_type="
+    "token&client_id=bb97b3e445a340b9b9cab4b9ea0dbd6f&scope=CommonInfo,ContactInfo,"
+    "FriendsAndRelatives,EducationalInfo"
+)
